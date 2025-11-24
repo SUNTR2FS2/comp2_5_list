b69d13b51245472730034672294ab8827e32c216
陽立 三反田 <ari19871976@outlook.jp>
2025-11-24 09:35:32 +0900

    テスト通しました

--- src/ConsoleApplication/ConsoleApplication.cpp
+++ src/ConsoleApplication/ConsoleApplication.cpp
@@ -1,28 +1,46 @@
-﻿// ConsoleApplication.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿// ConsoleApplication.cpp : このファイルには 'main' 関数が含まれています。
+// プログラム実行の開始と終了がそこで行われます。
+// ConsoleApplication.cpp : このファイルには 'main' 関数が含まれています。
+// プログラム実行の開始と終了がそこで行われます。
 
 #include <iostream>
+#include <cstdio>
 #include "../include/lib_func.h"
+
+// 素数判定関数
+bool is_prime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
 
 int main()
 {
-	// NUM までの素数を求める
-	const int NUM = 10000;
+    // NUM までの素数を求める
+    const int NUM = 10000;
 
-	list lst;
-	initialize_list(&lst);
+    list lst;
+    initialize_list(&lst);
 
-	node nd[NUM];
+    node nd[NUM]; // 素数だけを使うので NUM サイズ確保
 
-	for (int i = 1; i < NUM; i++) {
-		initialize_node(&nd[i], i+1);
-		push_front(&lst, &nd[i]);
-	}
+    int count = 0;
+    for (int i = 2; i <= NUM; i++) {
+        if (is_prime(i)) {
+            initialize_node(&nd[count], i);
+            push_front(&lst, &nd[count]);
+            count++;
+        }
+    }
 
-	node* p = lst.header;
-	while (p) {
-		printf("%d, ", p->data);
-		p = get_next(&lst, p);
-	}
-	printf("\n");
+    node* p = lst.header;
+    while (p) {
+        printf("%d, ", p->data);
+        p = get_next(&lst, p);
+    }
+    printf("\n");
+
+    return 0;
 }
--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -3,39 +3,41 @@
 
 #include "../include/lib_func.h"
 
-
 // ノードを初期化
 void initialize_node(node* p, int val)
 {
-	p->pNext = NULL;
-	p->data = val;
+    p->pNext = NULL;
+    p->data = val;
 }
 
 // リストを初期化
 void initialize_list(list* l)
 {
-	l->header = NULL;
+    l->header = NULL;
 }
-
 
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
-	// ToDo: ここにコードを追加
+    if (!l || !p) return;
+    p->pNext = l->header;   // 今の先頭を次にする
+    l->header = p;          // 新しいノードを先頭に
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
-	// ToDo: ここにコードを追加
+    if (!l || !p) return;
+    node* target = p->pNext;
+    if (target == NULL) return; // 次がない場合は何もしない
+    p->pNext = target->pNext;   // 次の次を繋げる
+    // メモリ解放は不要（テストではスタック変数を使っているため）
 }
-
 
 // pの次のノードを取得
 node* get_next(list* l, node* p)
 {
-	if (!p) return NULL;
-	return p->pNext;
-
-	return NULL;
+    (void)l; // lstは使わないが、インターフェースに含まれている
+    if (!p) return NULL;
+    return p->pNext;
 }
